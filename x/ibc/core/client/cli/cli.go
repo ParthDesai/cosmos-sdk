675340ba2a45414b22ecae5fcab161bae92105e3
Parth Desai <desaiparth08@gmail.com>
2021-03-02 18:58:19 +0530

    wire wasm light client correctly

--- x/ibc/core/client/cli/cli.go
+++ x/ibc/core/client/cli/cli.go
@@ -8,7 +8,8 @@ import (
 	connection "github.com/cosmos/cosmos-sdk/x/ibc/core/03-connection"
 	channel "github.com/cosmos/cosmos-sdk/x/ibc/core/04-channel"
 	host "github.com/cosmos/cosmos-sdk/x/ibc/core/24-host"
-	wasm "github.com/cosmos/cosmos-sdk/x/ibc/core/28-wasm"
+	wasmmanager "github.com/cosmos/cosmos-sdk/x/ibc/core/28-wasm"
+	wasm "github.com/cosmos/cosmos-sdk/x/ibc/light-clients/10-wasm"
 	solomachine "github.com/cosmos/cosmos-sdk/x/ibc/light-clients/06-solomachine"
 	tendermint "github.com/cosmos/cosmos-sdk/x/ibc/light-clients/07-tendermint"
 )
@@ -26,7 +27,7 @@ func GetTxCmd() *cobra.Command {
 	ibcTxCmd.AddCommand(
 		solomachine.GetTxCmd(),
 		tendermint.GetTxCmd(),
-		wasm.GetTxCmd(),
+		wasmmanager.GetTxCmd(),
 		connection.GetTxCmd(),
 		channel.GetTxCmd(),
 		wasm.GetTxCmd(),
@@ -50,7 +51,7 @@ func GetQueryCmd() *cobra.Command {
 		ibcclient.GetQueryCmd(),
 		connection.GetQueryCmd(),
 		channel.GetQueryCmd(),
-		wasm.GetQueryCmd(),
+		wasmmanager.GetQueryCmd(),
 	)
 
 	return ibcQueryCmd
